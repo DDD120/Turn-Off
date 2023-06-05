ec371cbc833fa805db81b06945896352d3541073
DDD120 <dltnals9794@naver.com>
2023-06-05 19:53:18 +0900

    feat meta 태그 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,14 @@ export const Head = () => (
     <meta name="Description" content="졸업작품 : 인터랙티브 웹툰" />
     <meta name="author" content="DDD120 : 이수민" />
     <title>Turn Off</title>
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Trun Off" />
+    <meta property="og:site_name" content="Trun Off" />
+    <meta property="og:description" content="졸업작품 : 인터랙티브 웹툰" />
+    <meta property="og:image" content="https://turn-off.netlify.app/og.png" />
+    <meta name="twitter:title" content="Trun Off" />
+    <meta name="twitter:description" content="졸업작품 : 인터랙티브 웹툰" />
+    <meta name="twitter:image" content="https://turn-off.netlify.app/og.png" />
   </>
 )
 
